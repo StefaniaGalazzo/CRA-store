ad93f9d897f72f4fd7358041e0cd35792e83f7eb
Stefania Galazzo <galazzostefania@gmail.com>
2021-12-20 18:55:17 +0100

    store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
+import { Products } from './Components/Home/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <header>
+        <h1>Store</h1>
+      </header>
+      <Products/>
     </div>
   );
 }
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import useFetch from "use-http";
+import style from "./Home.module.scss"
+
+function Products() {
+    const options = {} // these options accept all native fetch options
+    // the last argument below [] means it will fire onMount (GET by default)
+    const {data = [] } = useFetch('https://fakestoreapi.com/products', options, [])
+    return (
+    <div className={style.wrapper}>
+        {data.map(data => (<div key={data.id}>
+            <div className={style.product}>
+                <h3>{data.title}</h3>
+                <h4>{data.price} €</h4>
+                <img src={data.image} alt="im" />
+             </div>
+            </div>))}
+    </div>
+    )
+}
+
+export {Products};
